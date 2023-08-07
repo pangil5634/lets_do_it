40da12b5b19d758286ff291146a2a7a4fcaa5cf9
pangil5634 <oksk6681@naver.com>
2023-08-07 13:32:48 +0900

    230805 BaeJoon 18108 Complete

---
+++ devJava/baekJoon/P18108.java
@@ -0,0 +1,36 @@
+package devJava.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P18108 {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) {
+        P18108 main = new P18108();
+
+        main.run();
+    }
+
+    void run() {
+        getYear();
+    }
+
+    void getYear() {
+        int year = 0;
+        try {
+            year = Integer.parseInt(br.readLine());
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        year -= 543;
+        System.out.println(year);
+
+    }
+}
