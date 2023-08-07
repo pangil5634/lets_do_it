5cb489a97533f50029de81242f41cb7fc6d4b972
pangil5634 <oksk6681@naver.com>
2023-08-07 13:27:43 +0900

    230805 BaeJoon 10926 Complete

---
+++ devJava/baekJoon/P10926.java
@@ -0,0 +1,22 @@
+package devJava.baekJoon;
+
+import java.util.Scanner;
+
+class P10926 {
+
+	String input;
+	Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		P10926 main = new P10926();
+
+		main.getInput();
+	}
+
+	void getInput() {
+		input = sc.next();
+		String result = input + "??!";
+		System.out.println(result);
+	}
+
+}
