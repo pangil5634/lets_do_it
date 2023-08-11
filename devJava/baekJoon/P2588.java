dbc8f457612b58b7a8a4b44a9b74296a441b8817
pangil5634 <oksk6681@naver.com>
2023-08-11 12:57:23 +0900

    230811 Baekjoon P2588 Complete

---
+++ devJava/baekJoon/P2588.java
@@ -0,0 +1,41 @@
+package devJava.baekJoon;
+
+import java.util.Scanner;
+
+public class P2588 {
+
+    int a, b;
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        P2588 main = new P2588();
+
+        main.run();
+    }
+
+    void run() {
+        getNumber();
+
+        printResult();
+    }
+
+    void getNumber() {
+        a = sc.nextInt();
+        b = sc.nextInt();
+    }
+
+    void printResult() {
+        int hund = b / 100;
+        int ten = (b / 10) - (hund * 10);
+        int one = b % 10;
+
+        System.out.println(a * one);
+        System.out.println(a * ten);
+        System.out.println(a * hund);
+
+        System.out.println((a * one) + (a * ten * 10) + (a * hund * 100));
+
+    }
+
+}
