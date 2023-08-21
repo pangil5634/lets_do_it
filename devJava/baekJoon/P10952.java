b7c01839e172679803a9cb4b2ecf9d7e55fefe6d
pangil5634 <oksk6681@naver.com>
2023-08-21 22:54:49 +0900

    230821 BaekJoon P10951 P10952 P11654 P11720 P25083 P27866 Complete

---
+++ devJava/baekJoon/P10952.java
@@ -0,0 +1,22 @@
+package devJava.baekJoon;
+
+import java.util.Scanner;
+
+public class P10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+
+            if (n1 == 0 && n2 == 0)
+                break;
+
+            if (0 < n1 && n1 < 10 && 0 < n2 && n2 < 10)
+                System.out.println(n1 + n2);
+
+        }
+    }
+}
