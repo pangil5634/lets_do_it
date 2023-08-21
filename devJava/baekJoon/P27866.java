3633220d3dc2e6c49b7d16d8fdbc18a039da7f5a
pangil5634 <oksk6681@naver.com>
2023-08-21 22:54:49 +0900

    230821 BaekJoon P10951 P10952 P11654 P11720 P25083 P27866 Complete

---
+++ devJava/baekJoon/P27866.java
@@ -0,0 +1,13 @@
+package devJava.baekJoon;
+
+import java.util.Scanner;
+
+public class P27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int index = sc.nextInt();
+
+        System.out.println(str.charAt(index - 1));
+    }
+}
