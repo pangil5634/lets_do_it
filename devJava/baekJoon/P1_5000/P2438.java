4cfabb58a50e179ffc611fd7d298af4d1d152ab7
pangil5634 <oksk6681@naver.com>
2023-08-24 10:24:57 +0900

    230824 BaekJoon Solved

--- devJava/baekJoon/P1_5000/P2438.java
+++ devJava/baekJoon/P1_5000/P2438.java
@@ -4,8 +4,14 @@ import java.util.Scanner;
 
 public class P2438 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+        P2438 main = new P2438();
+        main.run();
+    }
+
+    Scanner sc = new Scanner(System.in);
+
+    void run() {
         int count = sc.nextInt();
 
         for (int i = 0; i < count; i++) {
