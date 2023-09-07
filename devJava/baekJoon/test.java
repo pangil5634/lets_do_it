5dc432d25a1831593b48148d39b9e62c21c17988
pangil5634 <oksk6681@naver.com>
2023-09-07 16:04:49 +0900

    230907 test case

--- devJava/baekJoon/test.java
+++ devJava/baekJoon/test.java
@@ -1,7 +1,38 @@
 package devJava.baekJoon;
 
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("good");
+        String input = "";
+
+        Scanner sc = new Scanner(System.in);
+
+        input = sc.nextLine();
+
+        String[] temps = input.split("");
+
+        String[] data = { "H", "C", "N", "O" };
+
+        int[] dataNumber = { 1, 12, 14, 16 };
+        int indexData = 0;
+        int total = 0;
+
+        for (int i = 0; i < temps.length; i++) {
+            if (temps[i].matches("[+-?]?\\d*(\\.\\d+)?")) {
+                total += dataNumber[indexData];
+                continue;
+            } else {
+                for (int j = 0; j < 4; j++) {
+
+                    if (temps[i].equals(data[j])) {
+                        indexData = j;
+                        break;
+                    }
+                }
+
+            }
+        }
+        System.out.println(total);
     }
 }
