13ed590e4c4c30ece8a5617d918da921634b3ddd
pangil5634 <oksk6681@naver.com>
2023-09-24 15:08:33 +0900

    230924 solved leetcode P1

---
+++ devJava/baekJoon/LJM/2023Lab04/src/edu/handong/csee/java/lab04/Cat.java
@@ -0,0 +1,14 @@
+package edu.handong.csee.java.lab04;
+
+public class Cat extends Animal {
+    private int countOfChew;
+
+    public int getCountOfChew() {
+        return countOfChew;
+    }
+
+    public void setCountOfChew(int countOfChew) {
+        this.countOfChew = countOfChew;
+    }
+
+}
---
+++ devJava/baekJoon/LJM/2023Lab04/src/edu/handong/csee/java/lab04/Dog.java
@@ -0,0 +1,14 @@
+package edu.handong.csee.java.lab04;
+
+public class Dog extends Animal {
+    private int dogGum;
+
+    public int getDogGum() {
+        return dogGum;
+    }
+
+    public void setDogGum(int dogGum) {
+        this.dogGum = dogGum;
+    }
+
+}
