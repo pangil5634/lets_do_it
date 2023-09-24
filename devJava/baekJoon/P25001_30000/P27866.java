851fe441ddc29fb3675388a5adc3a6f99b450aa8
pangil5634 <oksk6681@naver.com>
2023-09-24 15:09:23 +0900

    230924 baekJoon modified

--- devJava/baekJoon/P25001_30000/P27866.java
+++ devJava/baekJoon/P25001_30000/P27866.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class P27866 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int index = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            String str = sc.next();
+            int index = sc.nextInt();
 
-        System.out.println(str.charAt(index - 1));
+            System.out.println(str.charAt(index - 1));
+        }
     }
 }
--- devJava/baekJoon/P5001_10000/P5597.java
+++ devJava/baekJoon/P5001_10000/P5597.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 
 public class P5597 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            HashSet<Integer> data = new HashSet<>();
 
-        HashSet<Integer> data = new HashSet<>();
+            for (int i = 0; i < 28; i++) {
+                data.add(sc.nextInt());
+            }
 
-        for (int i = 0; i < 28; i++) {
-            data.add(sc.nextInt());
-        }
-
-        int max = 0;
-        int min = 30;
-        for (int i = 0; i < 30; i++) {
-            if (!data.contains(i + 1)) {
-                if (i + 1 < min) {
-                    min = i + 1;
-                }
-                if (i + 1 > max) {
-                    max = i + 1;
+            int max = 0;
+            int min = 30;
+            for (int i = 0; i < 30; i++) {
+                if (!data.contains(i + 1)) {
+                    if (i + 1 < min) {
+                        min = i + 1;
+                    }
+                    if (i + 1 > max) {
+                        max = i + 1;
+                    }
                 }
             }
-        }
 
-        System.out.println(min);
-        System.out.println(max);
+            System.out.println(min);
+            System.out.println(max);
+        }
     }
 }
