09305cfea6e99f25672e766385a6a072d758dc0c
pangil5634 <oksk6681@naver.com>
2023-11-02 16:25:12 +0900

    231102 study commit

---
+++ c++/test/solmi2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+int main(void)
+{
+
+    // 두 개의 수 선언
+    int num1 = 0;
+    int num2 = 0;
+
+    // 두 개의 수 입력 받기
+    cin >> num1;
+    cin >> num2;
+
+    // 나눌 값을 1~100의 자리 수로 나누기
+    int hund1 = num2 / 100;
+    int ten1 = num2 / 10 - (hund1 * 10);
+    int one1 = num2 - (hund1 * 100) - (ten1 * 10);
+
+    // 계산하기
+    int result = 0;
+
+    result += one1 * num1;
+    result += ten1 * num1 * 10;
+    result += hund1 * num1 * 100;
+
+    // 결과 출력
+    cout << one1 * num1 << endl;
+    cout << ten1 * num1 << endl;
+    cout << hund1 * num1 << endl;
+
+    cout << result << endl;
+    return 0;
+}
