6ea02982c049d1240633a89179b9a99835066bdd
pangil5634 <oksk6681@naver.com>
2023-11-06 11:23:15 +0900

    231106 하웹설 11주차 실습 끝

--- Web/html/하웹설/week11/class/myScript.js
+++ Web/html/하웹설/week11/class/myScript.js
@@ -1,6 +1,6 @@
 // id가 alert로 된 버튼을 click하면 경고창을 띄운다.
 $('#alert').click(function () {
-	alert("Button Clicked!");
+    alert("Button Clicked!");
 });
 
 // id가 button2를 클릭하면 id가 alert로 된 버튼을 click하는 행동을 취한다.
@@ -9,9 +9,8 @@ $("#button2").click(function () {
 })
 
 // class가 demo-container인 div 태그를 찾아서 해당 html 코드를 경고창으로 띄운다.
-// alert($("div.demo-container").html());
-
-// id가 btn으로 된 버튼을 클릭하면 class가 demo-container로 된 div 태그의 내부 html 값을 바꾼다.
+// alert($("div.demo-container").html()); id가 btn으로 된 버튼을 클릭하면 class가
+// demo-container로 된 div 태그의 내부 html 값을 바꾼다.
 $('#btn').click(function () {
     $('div.demo-container').html("<div class = 'demo-box'>Button clicked!</div>");
 })
@@ -24,5 +23,25 @@ $('#btn2').click(function () {
 // id가 btn3로 된 버튼을 클릭하면, class가 demo-box로된 div 태그를 삭제한다.
 $('#btn3').click(function () {
     $('div.demo-box').remove();
-})
+});
 
+$("#btn4").click(function () {
+    $("div.demo-box").empty();
+});
+
+$("#btn5").click(function () {
+    $("div.demo-box2").append("<p>Append Sample</p>");
+});
+
+$("#red-btn").click(function () {
+    $("div.addClass").removeClass("yellow");
+    $("div.addClass").addClass('red');
+});
+
+$("#yellow-btn").click(function () {
+    $("div.addClass").removeClass("red");
+    $("div.addClass").addClass('yellow');
+});
+
+$("#inputBox").val("Hi, hi");
+$("#selectBox").val(3)
