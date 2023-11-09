40fb02ce94982721446f8fb16256c0e0f3a8080a
pangil5634 <oksk6681@naver.com>
2023-11-09 15:40:34 +0900

    231109 test commit

---
+++ c/test/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int competency = 50; // 업무 능력
+    printf("Power : % d\n", competency);
+    // Power : 50
+
+    int comfortable = 50; // 편안함
+    printf("Power : % d\n", competency + comfortable);
+    // Power : 100
+}
