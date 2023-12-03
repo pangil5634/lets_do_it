8e1732f0bc5a3c0e22d4bbcc5bd0964cd95b582a
pangil5634 <oksk6681@naver.com>
2023-12-03 12:40:16 +0900

    231203 폴더 정리

--- Web/html/하웹설/week12/node/debug.js
+++
@@ -1,7 +0,0 @@
-var bool = false;
-
-if (bool) {
-    console.log('안녕하세요');
-} else {
-    console.log('에러 발생');
-}
--- Web/html/하웹설/week12/node/program.js
+++
@@ -1,4 +0,0 @@
-var calculator = require('./module.js')
-
-console.log(calculator.add(1, 2));
-console.log(calculator.sub(5, 3));
