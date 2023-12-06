afe4345f9afbfe762c5d31cf07534d66f8cd5b51
pangil5634 <oksk6681@naver.com>
2023-12-06 23:43:21 +0900

    231206 Page 컴포넌트 생성

---
+++ Web/myreact/src/components/Page.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Page = styled.div `
+    width : 100%;
+    height : 100vh;
+    box-sizing: border-box;
+    padding : 20px;
+    background-color: #FFF7D4;
+    color : #527853;
+`;
