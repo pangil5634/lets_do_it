9b1859c0d8b428f4f6300107468bf2c4c7259eb8
pangil5634 <oksk6681@naver.com>
2023-12-08 05:44:58 +0900

    231208 add 기능 구현

---
+++ Web/myreact/src/pages/AddPage.js
@@ -0,0 +1,65 @@
+import {Page} from "../components/Page";
+import {useState} from "react";
+import {db} from "../firebase";
+import {collection, addDoc} from "firebase/firestore";
+import {useFirebase} from "../App";
+import {Link, useNavigate} from "react-router-dom"; // useNavigate 추가
+
+function AddPage() {
+    const {posts, setPosts} = useFirebase();
+    const [postData, setPostData] = useState({title: "", content: ""});
+    const navigate = useNavigate(); // useNavigate 훅 사용
+
+    const addPost = async () => {
+        if (postData.title && postData.content) {
+            try {
+                const docRef = await addDoc(collection(db, "posts"), postData);
+                alert("데이터가 성공적으로 추가되었습니다");
+
+                setPosts([
+                    ...posts, {
+                        id: docRef.id,
+                        ...postData
+                    }
+                ]);
+
+                // 추가 후 Post 페이지로 이동
+                navigate(`/post`);
+            } catch (error) {
+                console.error("데이터 추가 중 오류 발생:", error);
+            }
+        } else {
+            alert("유효한 데이터가 아닙니다.");
+        }
+    };
+
+    return (
+        <Page>
+            등록 페이지입니다.
+            <div>
+                <label>title:</label>
+                <input
+                    type="text"
+                    name="title"
+                    onChange={(e) => setPostData({
+                        ...postData,
+                        title: e.target.value
+                    })}/>
+                <br/>
+                <label>content :</label>
+                <input
+                    type="text"
+                    name="content"
+                    onChange={(e) => setPostData({
+                        ...postData,
+                        content: e.target.value
+                    })}/>
+                <br/>
+                <button onClick={addPost}>등록</button>
+
+            </div>
+        </Page>
+    );
+}
+
+export default AddPage;
--- Web/myreact/src/pages/Post.js
+++ Web/myreact/src/pages/Post.js
@@ -1,29 +1,49 @@
 import React from 'react';
-import { Page } from '../components/Page';
-import { useFirebase } from '../App'; // App 컴포넌트에서 만든 useFirebase 커스텀 훅을 가져옴
-import { Link } from 'react-router-dom';
-import { H2 } from "../components/Heading";
+import {Page} from '../components/Page';
+import {useFirebase} from '../App'; // App 컴포넌트에서 만든 useFirebase 커스텀 훅을 가져옴
+import {Link} from 'react-router-dom';
+import {H2} from "../components/Heading";
+import { deleteDoc, doc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function Home() {
-  // useFirebase 훅을 사용하여 Firebase 데이터에 접근
-  const { posts } = useFirebase();
+    // useFirebase 훅을 사용하여 Firebase 데이터에 접근
+    const {posts, setPosts} = useFirebase();
+    const deletePost = async (postId) => {
+        try {
+            const updatedPosts = posts.filter((post) => post.id !== postId);
+            setPosts(updatedPosts);
 
-  return (
-    <Page>
-      <h1>Post 페이지입니다.</h1>
-      
-      {/* posts 배열을 사용하여 데이터를 렌더링 */}
-      {posts.map((post) => (
-        <div key={post.id}>
-         {/* Link 컴포넌트를 사용하여 페이지 이동 */}
-          <Link to={`/post/${post.id}`}>
-            <H2 hoverColor = "skyblue">{post.title}</H2>
-          </Link>
-          {/* <p>{post.content}</p> */}
-        </div>
-      ))}
-    </Page>
-  );
+            console.log('Firestore에서 게시물 삭제 중:', postId);
+            await deleteDoc(doc(db, 'posts', postId));
+            console.log('Firestore에서 게시물이 성공적으로 삭제되었습니다.');
+        } catch (error) {
+            console.error("게시물 삭제 중 오류 발생:", error);
+        }
+    };
+
+    return (
+        <Page>
+            <h1>Post 페이지입니다.</h1>
+            <Link to="/AddPage">
+                <button>등록 페이지</button>
+            </Link>
+
+            {/* posts 배열을 사용하여 데이터를 렌더링 */}
+            {
+                posts.map((post) => (
+                    <div key={post.id}>
+                        {/* Link 컴포넌트를 사용하여 페이지 이동 */}
+                        <Link to={`/post/${post.id}`}>
+                            <H2 hoverColor="skyblue">{post.title}</H2>
+                        </Link>
+                        {/* 삭제 버튼 추가 */}
+                        <button onClick={() => deletePost(post.id)}>삭제</button>
+                    </div>
+                ))
+            }
+        </Page>
+    );
 }
 
 export default Home;
