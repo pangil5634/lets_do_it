6a0a384bddbf4669f739a49f2ba965d72d8721fd
pangil5634 <oksk6681@naver.com>
2023-12-07 22:56:00 +0900

    231207 Post/PostDetails 페이지 추가

---
+++ Web/myreact/src/pages/Post.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Page } from '../components/Page';
+import { useFirebase } from '../App'; // App 컴포넌트에서 만든 useFirebase 커스텀 훅을 가져옴
+import { Link } from 'react-router-dom';
+
+function Home() {
+  // useFirebase 훅을 사용하여 Firebase 데이터에 접근
+  const { posts } = useFirebase();
+
+  return (
+    <Page>
+      <h1>Post 페이지입니다.</h1>
+      
+      {/* posts 배열을 사용하여 데이터를 렌더링 */}
+      {posts.map((post) => (
+        <div key={post.id}>
+         {/* Link 컴포넌트를 사용하여 페이지 이동 */}
+          <Link to={`/post/${post.id}`}>
+            <h2>{post.title}</h2>
+          </Link>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </Page>
+  );
+}
+
+export default Home;
