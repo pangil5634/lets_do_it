f6abb8e5282e55680e8c0c706f773d8f0630331b
pangil5634 <oksk6681@naver.com>
2023-12-07 22:23:19 +0900

    231207 firebase 적용

--- Web/myreact/src/pages/HomePage.js
+++ Web/myreact/src/pages/HomePage.js
@@ -1,11 +1,24 @@
-import { Page } from "../components/Page"
+import React from 'react';
+import { Page } from '../components/Page';
+import { useFirebase } from '../App'; // App 컴포넌트에서 만든 useFirebase 커스텀 훅을 가져옴
+
 function Home() {
-    
-    return (
-      <Page>
-        <h1>홈페이지입니다.</h1>
-      </Page>
-    );
+  // useFirebase 훅을 사용하여 Firebase 데이터에 접근
+  const { posts } = useFirebase();
+
+  return (
+    <Page>
+      <h1>홈페이지입니다.</h1>
+      
+      {/* posts 배열을 사용하여 데이터를 렌더링 */}
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </Page>
+  );
 }
 
 export default Home;
