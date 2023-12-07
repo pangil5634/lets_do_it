bfd5cdda47eac892a039bc475ad00518dd9008f9
pangil5634 <oksk6681@naver.com>
2023-12-08 08:01:28 +0900

    231208 이미지 추가

--- Web/myreact/src/pages/HomePage.js
+++ Web/myreact/src/pages/HomePage.js
@@ -3,13 +3,14 @@ import React from 'react';
 import {Page} from '../components/Page';
 import {useFirebase} from '../App';
 import { UserName } from '../components/Menubar';
-
+import ImageCat from '../image/cat.png';
 function HomePage() {
     const {posts, setPosts} = useFirebase();
     const {user} = useFirebase();
 
     return (
         <Page>
+            <img src = {ImageCat}/>
             <h1>홈 페이지입니다.</h1>
             {user && <div><UserName>{user.displayName}</UserName>님 환영합니다.</div>}
         </Page>
