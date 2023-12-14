06da5da34b8ce39b8448d6bdc4cd5f41dec13c1c
pangil5634 <oksk6681@naver.com>
2023-12-14 14:29:57 +0900

    231214 주석 추가

--- Web/myreact/src/pages/PostDetails.js
+++ Web/myreact/src/pages/PostDetails.js
@@ -1,15 +1,15 @@
 // PostDeatils.js 파일
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useFirebase } from '../App';
-import { Page } from '../components/Page';
+import {useParams} from 'react-router-dom';
+import {useFirebase} from '../App';
+import {Page} from '../components/Page';
 
 function PostDetails() {
     // Firebase 컨텍스트에서 데이터 가져오기
-    const { posts } = useFirebase();
+    const {posts} = useFirebase();
 
     // URL의 동적 매개변수에서 postId 추출
-    const { postId } = useParams();
+    const {postId} = useParams();
 
     // postId에 해당하는 포스트 찾기
     const post = posts.find((p) => p.id === postId);
@@ -25,15 +25,25 @@ function PostDetails() {
                 backgroundColor="white"
                 alignItems="center"
                 borderRadius="20px"
-                height="100%"
-            >
+                height="100%">
+
+                {/* 게시글 제목 */}
                 <h1>{post.title}</h1>
+                {/* 게시글 내용 */}
                 <p>{post.content}</p>
-                {post.imageUrl && (
-                    <div>
-                        <img src={post.imageUrl} alt="Post" style={{ maxWidth: '100%' }} />
-                    </div>
-                )}
+                {/* 게시글 이미지 */}
+                {
+                    post.imageUrl && (
+                        <div>
+                            <img
+                                src={post.imageUrl}
+                                alt="Post"
+                                style={{
+                                    maxWidth: '100%'
+                                }}/>
+                        </div>
+                    )
+                }
             </Page>
         </Page>
     );
