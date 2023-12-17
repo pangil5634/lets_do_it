63a2e4612c598bc0bbd3144a0f55cedf56a40b2a
pangil5634 <oksk6681@naver.com>
2023-12-17 23:01:14 +0900

    231217 MyPage.js 주석 추가

--- Web/myreact/src/pages/MyPage.js
+++ Web/myreact/src/pages/MyPage.js
@@ -8,7 +8,10 @@ function MyPage() {
                 backgroundColor="white"
                 alignItems="center"
                 borderRadius="20px">
+                {/* 타이틀 */}
                 <h1>마이페이지입니다.</h1>
+
+                {/* 컨텐츠 */}
                 <p>내용이 업데이트 될 예정입니다.</p>
             </Page>
         </Page>
