fb339323166890badd046dc3bb37129baa0d9c53
pangil5634 <oksk6681@naver.com>
2023-12-17 23:02:00 +0900

    231217 HomePage.js 주석 추가

--- Web/myreact/src/pages/HomePage.js
+++ Web/myreact/src/pages/HomePage.js
@@ -15,7 +15,11 @@ function HomePage() {
 
     return (
         <Page alignItems="center">
-            <Page width="90%" backgroundColor="white" alignItems="center" borderRadius = "20px">
+            <Page
+                width="90%"
+                backgroundColor="white"
+                alignItems="center"
+                borderRadius="20px">
                 {/* 로그인 시 보여지는 유저 이름 */}
                 {
                     user && <div>
@@ -25,16 +29,14 @@ function HomePage() {
                 {/* 페이지의 타이틀*/}
                 <P fontSize="5vw" fontWeight="bold">팡팡 터지는 팡일이의 WebSite</P>
 
-                {/*  */}
-                <FieldSet width = "90%"  fontSize="20px">
-                    <Legend
-                        fontSize = "30px"
-                        fontWeight = "bold"
-                        color = "black"
-                    >자기소개</Legend>
-                    <p>안녕하세요! 즐거운 개발을 추구하는 <strong>프론트엔드 개발자</strong>가 되고 싶은 김광일입니다.</p>
-                    
-                    <p>현재 저는 한동대학교 전산전자공학부에 재학중이며, <strong>웹 개발</strong>을 주전공으로 갖기 위해 노력하고 있습니다.</p>
+                {/* 페이지의 컨텐츠 */}
+                <FieldSet width="90%" fontSize="20px">
+                    <Legend fontSize="30px" fontWeight="bold" color="black">자기소개</Legend>
+                    <p>안녕하세요! 즐거운 개발을 추구하는
+                        <strong>프론트엔드 개발자</strong>가 되고 싶은 김광일입니다.</p>
+
+                    <p>현재 저는 한동대학교 전산전자공학부에 재학중이며,
+                        <strong>웹 개발</strong>을 주전공으로 갖기 위해 노력하고 있습니다.</p>
 
                     <p>부족한 실력이지만 자기소개 페이지 개발을 시작으로 포토폴리오를 만들어나가보려고 합니다.</p>
 
