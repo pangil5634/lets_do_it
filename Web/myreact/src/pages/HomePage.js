9b0bd8f6851e7c2e1392d28312fff97363f5e3da
pangil5634 <oksk6681@naver.com>
2023-12-24 21:26:04 +0900

    231224 이미지 require로 방법 수정

--- Web/myreact/src/pages/HomePage.js
+++ Web/myreact/src/pages/HomePage.js
@@ -9,12 +9,6 @@ import {Page, FieldSet, Legend, Img, Label} from '../components/Page';
 import {Div, UserName} from '../components/Menubar';
 import {P} from '../components/Heading';
 
-import imageCSS from '../image/PL/css.png';
-import imageHTML from '../image/PL/html.png';
-import imageJS from '../image/PL/javascript.png';
-import imageJAVA from '../image/PL/java.png';
-import imageGIT from '../image/PL/git.png';
-
 function HomePage() {
     // 변수 선언
     const {user} = useFirebase();
@@ -38,10 +32,10 @@ function HomePage() {
                 {/* 페이지의 컨텐츠 */}
                 <FieldSet width="90%" fontSize="20px">
                     <Legend fontSize="30px" fontWeight="bold" color="black">자기소개</Legend>
-                    <p>안녕하세요! 즐거운 개발을 추구하는
+                    <p>안녕하세요! 즐거운 개발을 추구하는&nbsp;
                         <strong>프론트엔드 개발자</strong>가 되고 싶은 김광일입니다.</p>
 
-                    <p>현재 저는 한동대학교 전산전자공학부에 재학중이며,
+                    <p>현재 저는 한동대학교 전산전자공학부에 재학중이며,&nbsp;
                         <strong>웹 개발</strong>을 주전공으로 갖기 위해 노력하고 있습니다.</p>
 
                     <p>부족한 실력이지만 자기소개 페이지 개발을 시작으로 포토폴리오를 만들어나가보려고 합니다.</p>
@@ -53,27 +47,27 @@ function HomePage() {
                 <br/> {/* 페이지의 컨텐츠 */}
                 <FieldSet width="90%" fontSize="20px">
                     <Legend fontSize="30px" fontWeight="bold" color="black">개발 stack</Legend>
-                    <P textAlign = "center">이미지에 마우스를 올리면 BLUR처리 됩니다.</P>
+                    <P textAlign="center">이미지에 마우스를 올리면 BLUR처리 됩니다.</P>
                     <br/>
                     <Div justifyContent="space-between">
                         <Div flexDirection="column">
-                            <Img src={imageHTML} alt="html" blur={true} />
+                            <Img src={require('../image/PL/html.png')} alt="html" blur={true}/>
                             <Label color="black">html</Label>
                         </Div>
                         <Div flexDirection="column">
-                            <Img src={imageCSS} alt="css" blur={true} />
+                            <Img src={require('../image/PL/css.png')} alt="css" blur={true}/>
                             <Label color="black">css</Label>
                         </Div>
                         <Div flexDirection="column">
-                            <Img src={imageJS} alt="js" blur={true} />
+                            <Img src={require('../image/PL/javascript.png')} alt="js" blur={true}/>
                             <Label color="black">javascript</Label>
                         </Div>
                         <Div flexDirection="column">
-                            <Img src={imageJAVA} alt="java" blur={true}/>
+                            <Img src={require('../image/PL/java.png')} alt="java" blur={true}/>
                             <Label color="black">java</Label>
                         </Div>
                         <Div flexDirection="column">
-                            <Img src={imageGIT} alt="git" blur={true}/>
+                            <Img src={require('../image/PL/git.png')} alt="git" blur={true}/>
                             <Label color="black">git</Label>
                         </Div>
 
