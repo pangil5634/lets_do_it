081b0548c0f3a4f84d99b284d22ac0efc3bfae14
pangil5634 <oksk6681@naver.com>
2024-04-24 20:36:06 +0900

    edit : MyRoute

--- Web/myreact/src/MyRoute.js
+++ Web/myreact/src/MyRoute.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./Pages/HomePage/HomePage";
 import Layout from "./Layout/Layout";
+
+import HomePage from "./Pages/HomePage/HomePage"
 
 function MyRoute() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element = {<Layout/>}>
-                    <Route index path="/" element={<HomePage />} />
+                    <Route index path="/" element={<HomePage/>} />
                 </Route>
             </Routes>
         </BrowserRouter>
